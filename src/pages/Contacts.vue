d86fbc1e83f88997060801bbfa0f342ccfc983aa
peppegerace <peppegerace47@gmail.com>
2024-11-06 10:54:12 +0100

    [add] contacts

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -5,10 +5,37 @@ export default {
 </script>
 
 <template>
-  <h1>contatti</h1>
+  <div class="contacts">
+    <div class="container">
+      <h2>I miei contatti:</h2>
+      <ul class="ps-0">
+        <li>
+          <a href="">
+            <i class="fa-brands fa-linkedin me-3"></i>
+            <span>Giuseppe Gerace</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i class="fa-solid fa-phone me-3"></i>
+            <span>+39 3402612589</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <i class="fa-solid fa-at me-3"></i>
+            <span>giuseppe.gerace95@gmail.com</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
 @use '../scss/main.scss' as *;
 
+.contacts a {
+  color: $secondary-color;
+}
 </style>
